766b415b42c8b8adeb0fcc6bea0e0832c655f56a
Yaroslav Kondrasiuk <yvkondrasiuk@gmail.com>
2023-11-03 05:08:54 +0200

    repository, services and main

--- src/main/java/org/example/repositories/ProducerRepository.java
+++ src/main/java/org/example/repositories/ProducerRepository.java
@@ -1,11 +1,8 @@
 package org.example.repositories;
 
 import org.example.entities.Producer;
-import org.example.entities.Souvenir;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public interface ProducerRepository {
     void createProducer(Producer producer);
